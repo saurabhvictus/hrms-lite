d5285e5ca14c687589f299e5796c2eaa40e996bd
Saurabh Singh <supersaurabh98@gmail.com>
2026-02-12 01:40:12 +0530

    post error fixed

--- backend/app/routes/employee.py
+++ backend/app/routes/employee.py
@@ -6,17 +6,21 @@ router = APIRouter(prefix="/employees", tags=["Employees"])
 
 @router.post("", status_code=201)
 def create_employee(emp: EmployeeCreate):
-    if employee_collection.find_one({
-        "$or": [
-            {"employeeId": emp.employeeId},
-            {"email": emp.email}
-        ]
-    }):
-        raise HTTPException(409, "Employee already exists")
+    try:
+        if employee_collection.find_one({
+            "$or": [
+                {"employeeId": emp.employeeId},
+                {"email": emp.email}
+            ]
+        }):
+            raise HTTPException(409, detail="Employee already exists")
 
-    employee_collection.insert_one(emp.model_dump())
+        employee_collection.insert_one(emp.model_dump())
 
-    return {"message": "Employee added"}
+        return {"message": "Employee added"}
+    except Exception as e:
+        print("POST /employees ERROR:", e)
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("")
 def get_employees():
